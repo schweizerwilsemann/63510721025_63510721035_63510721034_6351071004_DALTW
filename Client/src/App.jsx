9c73e5fc0179ff8f77763ad54ed9bcec723a9719
hoang95633 <leminhoang2004@gmail.com>
2024-10-02 09:48:37 +0700

    detail page

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,16 +1,19 @@
-import { useState } from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import ScrollToTop from "./components/ScrollToTop";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import Home from "./pages/Home";
-import SignIn from "./pages/SignIn";
-import SignUp from "./pages/SignUp";
-import About from "./pages/About";
-import Search from "./pages/Search";
-import Projects from "./pages/Projects";
-import { toast, ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+import { useState } from 'react'
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom'
+import ScrollToTop from './components/ScrollToTop'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Home from './pages/Home'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import About from './pages/About'
+import Search from './pages/Search'
+import Projects from './pages/Projects'
+import { toast, ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+
+// demo
+import BookDetail from './pages/BookDetail'
 
 function App() {
   return (
@@ -19,11 +22,13 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/books" element={<Navigate to="/" />} />
         <Route path="/about" element={<About />} />
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/search" element={<Search />} />
         <Route path="/projects" element={<Projects />} />
+        <Route path="/books/harry-potter" element={<BookDetail />} />
       </Routes>
       <Footer />
       <ToastContainer
@@ -39,7 +44,7 @@ function App() {
         theme="light"
       />
     </BrowserRouter>
-  );
+  )
 }
 
-export default App;
+export default App
