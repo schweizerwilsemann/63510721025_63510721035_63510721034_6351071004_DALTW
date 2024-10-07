b1fb1dbc8eda142e5192593bef4e886825a23801
project_Html <khoanguyenduc99@gmail.com>
2024-10-07 18:38:28 +0700

    no need

--- Apis/obj/Debug/net8.0/apis.AssemblyInfo.cs
+++ Apis/obj/Debug/net8.0/apis.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("apis")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9f10b7afdebe7d23c298f55b516f29ff18f7dec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8952a8567b8398abdee15f384799277ec9e074fe")]
 [assembly: System.Reflection.AssemblyProductAttribute("apis")]
 [assembly: System.Reflection.AssemblyTitleAttribute("apis")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
