a50324ac5afd5496b28bb36ed7daf5758a98ab4c
Hoang Le <you@example.com>
2024-10-08 18:16:30 +0700

    detail page 8/10

--- Client/tailwind.config.js
+++ Client/tailwind.config.js
@@ -1,10 +1,10 @@
 /** @type {import('tailwindcss').Config} */
-const flowbite = require("flowbite-react/tailwind");
+const flowbite = require('flowbite-react/tailwind')
 
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}", flowbite.content()],
-  theme: {
-    extend: {},
-  },
-  plugins: [require("tailwind-scrollbar"), flowbite.plugin()],
-};
+    content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}', flowbite.content()],
+    theme: {
+        extend: {},
+    },
+    plugins: [require('tailwind-scrollbar'), flowbite.plugin(), require('@tailwindcss/line-clamp')],
+}
