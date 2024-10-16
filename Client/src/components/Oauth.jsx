f564422364963c2df3de1b2edbfd7f41b3fb671d
hoang95633 <leminhoang2004@gmail.com>
2024-10-16 13:31:55 +0700

    oauth

--- Client/src/components/Oauth.jsx
+++ Client/src/components/Oauth.jsx
@@ -1,43 +1,42 @@
-import { Button } from "flowbite-react";
-import React from "react";
-import { AiFillGoogleCircle } from "react-icons/ai";
-import { GoogleAuthProvider, signInWithPopup, getAuth } from "firebase/auth";
-import { app } from "../firebase";
-import { useDispatch } from "react-redux";
-import { signInSuccess } from "../redux/user/UserSlice";
-import { useNavigate } from "react-router-dom";
-
+import { Button } from 'flowbite-react'
+import React from 'react'
+import { AiFillGoogleCircle } from 'react-icons/ai'
+import { GoogleAuthProvider, signInWithPopup, getAuth } from 'firebase/auth'
+import { app } from '../firebase'
+import { useDispatch } from 'react-redux'
+import { signInSuccess } from '../redux/user/UserSlice'
+import { useNavigate } from 'react-router-dom'
 export default function OAuth() {
-  const auth = getAuth(app);
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
+  const auth = getAuth(app)
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
   const handleGoogleClick = async () => {
-    const provider = new GoogleAuthProvider();
+    const provider = new GoogleAuthProvider()
 
     provider.setCustomParameters({
-      prompt: "select_account",
-    });
+      prompt: 'select_account',
+    })
     try {
-      const resultsFromGoogle = await signInWithPopup(auth, provider);
-      const res = await fetch("/api/auth/google-login", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
+      const resultsFromGoogle = await signInWithPopup(auth, provider)
+      const res = await fetch('/api/auth/google-login', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
           name: resultsFromGoogle.user.displayName,
           email: resultsFromGoogle.user.email,
           googlePhotoURL: resultsFromGoogle.user.photoURL,
         }),
-      });
-      const data = await res.json();
+      })
+      const data = await res.json()
       if (res.ok) {
-        localStorage.setItem("token", data.token);
-        dispatch(signInSuccess(data.user));
-        navigate("/");
+        localStorage.setItem('token', data.token)
+        dispatch(signInSuccess(data.user))
+        navigate('/')
       }
     } catch (error) {
-      console.log(">>> Show Error: ", error);
+      console.log('>>> Show Error: ', error)
     }
-  };
+  }
   return (
     <Button
       className="w-full items-center justify-center mt-2"
@@ -49,5 +48,5 @@ export default function OAuth() {
       <AiFillGoogleCircle className="w-6 h-6 mr-2 text-stone-600" />
       Continue With Google
     </Button>
-  );
+  )
 }
--- Client/src/firebase.js
+++ Client/src/firebase.js
@@ -1,5 +1,6 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+// Import the functions you need from the SDKs ryou need
+import { initializeApp } from 'firebase/app'
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -7,13 +8,13 @@ import { initializeApp } from "firebase/app";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: "reading-book-web.firebaseapp.com",
-  projectId: "reading-book-web",
-  storageBucket: "reading-book-web.appspot.com",
-  messagingSenderId: "348486324876",
-  appId: "1:348486324876:web:1c2c2c5aada6d343b261a9",
-  measurementId: "G-7BTW8XZ8SW",
-};
+  authDomain: 'reading-book-web.firebaseapp.com',
+  projectId: 'reading-book-web',
+  storageBucket: 'reading-book-web.appspot.com',
+  messagingSenderId: '348486324876',
+  appId: '1:348486324876:web:1c2c2c5aada6d343b261a9',
+  measurementId: 'G-7BTW8XZ8SW',
+}
 
 // Initialize Firebase
-export const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig)
