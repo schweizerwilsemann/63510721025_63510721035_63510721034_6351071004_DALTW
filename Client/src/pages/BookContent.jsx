414b51bf667c327706d957a95660b1ad840b5d89
project_Html <khoanguyenduc99@gmail.com>
2025-02-17 12:01:10 +0700

    add css

--- Client/src/pages/BookContent.jsx
+++ Client/src/pages/BookContent.jsx
@@ -4,15 +4,14 @@ import HTMLFlipBook from "react-pageflip";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 import "react-pdf/dist/esm/Page/TextLayer.css";
 import Breadcrumb from "../components/Breadcrumb";
+import "../styles/BookContent.css"; // Make sure to import your CSS file
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@4.4.168/build/pdf.worker.min.mjs`;
 
 export const BookContent = () => {
   const [pdfUrl, setPdfUrl] = useState(null);
-  const [currentBookSlug, setCurrentBookSlug] = useState("");
   const [numPages, setNumPages] = useState(null);
   const [loading, setLoading] = useState(false);
-  const [chapter, setChapter] = useState(0);
   const [book, setBook] = useState({});
   const onDocumentLoadSuccess = (pdf) => {
     setNumPages(pdf.numPages);
@@ -59,13 +58,17 @@ export const BookContent = () => {
   return (
     <>
       <Breadcrumb book={book} pageNumber={numPages} />
-      <div className="flex justify-center items-center h-30rem  bg-gray-100">
+      <div className="flex justify-center items-center h-30rem bg-gray-100">
         {pdfUrl ? (
           <HTMLFlipBook width={600} height={800} className="shadow-lg">
             {Array.from(new Array(numPages), (el, index) => (
               <div key={`page_${index + 1}`} className="p-4">
                 <Document file={pdfUrl} onLoadSuccess={onDocumentLoadSuccess}>
-                  <Page pageNumber={index + 1} scale={1.5} />
+                  <Page
+                    pageNumber={index + 1}
+                    scale={1.5}
+                    className="pdf-page"
+                  />
                 </Document>
               </div>
             ))}
